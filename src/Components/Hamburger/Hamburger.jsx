7b8bf483e53b626524301082986f20be19e122c9
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2023-09-23 17:32:25 +0200

    First commit

---
+++ src/Components/Hamburger/Hamburger.jsx
@@ -0,0 +1,15 @@
+import "./Hamburger.modale.css";
+
+export function Hamburger() {
+  return (
+    <div
+      id="ham-container"
+      className="ham-items"
+      onClick={function CreateMenu() {}}
+    >
+      <div className="ham ham1 ham-item1"></div>
+      <div className="ham ham2 ham-item2"></div>
+      <div className="ham ham3 ham-item3"></div>
+    </div>
+  );
+}
