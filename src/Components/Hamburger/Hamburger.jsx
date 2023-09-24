a90746e2fece5a98ce27f6adeaf884e1936829ec
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2023-09-24 11:02:56 +0200

    Making menu work

--- src/Components/Hamburger/Hamburger.jsx
+++ src/Components/Hamburger/Hamburger.jsx
@@ -1,15 +1,20 @@
 import "./Hamburger.modale.css";
+import { Menu } from "../Menu/Menu";
+import { useState } from "react";
 
 export function Hamburger() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (
-    <div
-      id="ham-container"
-      className="ham-items"
-      onClick={function CreateMenu() {}}
-    >
+    <div id="ham-container" className="ham-items" onClick={toggleMenu}>
       <div className="ham ham1 ham-item1"></div>
       <div className="ham ham2 ham-item2"></div>
       <div className="ham ham3 ham-item3"></div>
+      {menuVisible && <Menu />} {}
     </div>
   );
 }
--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,9 +1,9 @@
 export function Menu() {
   return (
     <div id="menu-container" className="menu-items">
-      <div className="menu menu1 menu-item1 menu"></div>
-      <div className="menu menu2 menu-item2 menu"></div>
-      <div className="menu menu3 menu-item3 menu"></div>
+      <div className="menu menu1 menu-item1 menu">About</div>
+      <div className="menu menu2 menu-item2 menu">Porjects</div>
+      <div className="menu menu3 menu-item3 menu">Contact</div>
     </div>
   );
 }
