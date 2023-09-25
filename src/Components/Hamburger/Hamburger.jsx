0cc853b8e795b584ea6ccaaa9e31ea1d786890d9
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2023-09-25 07:10:44 +0200

    Moving along

--- src/Components/Hamburger/Hamburger.jsx
+++ src/Components/Hamburger/Hamburger.jsx
@@ -4,17 +4,23 @@ import { useState } from "react";
 
 export function Hamburger() {
   const [menuVisible, setMenuVisible] = useState(false);
+  const [hamburgerVisible, setHamburgerVisible] = useState(true);
 
   const toggleMenu = () => {
     setMenuVisible(!menuVisible);
+    setHamburgerVisible(false);
   };
 
   return (
-    <div id="ham-container" className="ham-items" onClick={toggleMenu}>
-      <div className="ham ham1 ham-item1"></div>
-      <div className="ham ham2 ham-item2"></div>
-      <div className="ham ham3 ham-item3"></div>
+    <>
+      {hamburgerVisible && (
+        <div id="ham-container" className="ham-items" onClick={toggleMenu}>
+          <div className="ham ham1 ham-item1"></div>
+          <div className="ham ham2 ham-item2"></div>
+          <div className="ham ham3 ham-item3"></div>
+        </div>
+      )}
       {menuVisible && <Menu />} {}
-    </div>
+    </>
   );
 }
